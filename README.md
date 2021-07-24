7afb8ce1240cad242f77abc70625a43baa0e503f
rubigit <rcortes1708.rcj@gmail.com>
2021-07-23 21:48:32 -0400

    Add feature to provide random parameter to the API. Add code comments. Validate that providen names are letters

--- README.md
+++ README.md
@@ -1,14 +1,18 @@
 #   MANUAL - Love calculator
 
-##  Supported operations:
+##  Supported commands:
 ### Run love calculator
-Provide 'two names' as parameters: node app.js firstName secondName. 
-Example: `node app.js Lisa Paul`
-Note: The results are recorded in the 'history.txt' file
-
+Provide 'two names' as parameters: node app.js firstName secondName.
+- Example:  `node app.js Lisa Paul`
+Note:       The results are recorded in the 'history.txt' file
+### Run love calculator with random parameters
+Provide the command 'random' as parameter. 
+- Example: `node app.js random`
+Notes:     The parameters are taken from 'femalenames.txt' and 'malenames.txt' files
+           The results are recorded in the 'history.txt' file
 ### Print content of History file
 Provide the command 'history' as parameter. 
-Example: `node app.js history`
+- Example: `node app.js history`
 
 ##  Aditional notes:
 The 'log.txt' file keeps a record of application usage with supported or invalid commands.
--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //import libraries
 let fs = require("fs");
 let historyLib = require("./lib/history");
+let namesLib = require("./lib/rnames");
 let mathLib = require("./lib/math");
 const axios = require('axios');
 
@@ -8,7 +9,8 @@ const axios = require('axios');
 //create app object
 let app = {};
 
-// Append results from API love calculator to the history file
+
+// Append results from API(love calculator) to the history file
 app.historyfile = function (data1, data2, data3, data4) {
   fs.appendFile(
     __dirname + "/lib/history/history.txt",
@@ -21,7 +23,7 @@ app.historyfile = function (data1, data2, data3, data4) {
 }
 
 
-// Append supported commands used in the app to the log file
+// Append used commands to the log file
 app.logfile = function (command) {
   fs.appendFile(
     __dirname + "/log.txt",
@@ -35,8 +37,9 @@ app.logfile = function (command) {
 
 //Use library to retrieve content of history file
 app.printHistory = function () {
+  //Console operation in progress
   console.log(`> Printing History...`)
-  //Retrieve arrar: content of history file
+  //Retrieve array: content of history file
   let arrhisto = historyLib.arrhisto();
   let arrhistoLength = arrhisto.length;
   console.log(`-----------------------------------------------------------`)
@@ -54,15 +57,52 @@ app.printHistory = function () {
   app.consoleOptions()
 }
 
+//Use libraries to get a random male name from a file
+app.getMale = function () {
+  let arrMale = namesLib.arrMale()
+  let arrMaleLength = arrMale.length
+  let randomNumber = mathLib.getRandomNumber(0, arrMaleLength - 1)
+  //return a random male name
+  return arrMale[randomNumber]
+};
+
+//Use libraries to get a random male name from a file
+app.getFemale = function () {
+  let arrFemale = namesLib.arrFemale()
+  let arrFemaleLength = arrFemale.length
+  let randomNumber = mathLib.getRandomNumber(0, arrFemaleLength - 1)
+  //return a random female name
+  return arrFemale[randomNumber]
+};
+
 //Call the API love calculator
 //Use axios request
-app.calculateLove = function () {
-  console.log(`> Calculating love ...`)
+app.calculateLove = function (rCommand) {
+
+  //initialize variables
+  let firstname = ``
+  let secondname = ``
+  let randomlove = ``
+
+  //Assigne random names 
+  if (rCommand) {
+    firstname = app.getMale()
+    secondname = app.getFemale()
+    randomlove = rCommand + ` `
+  }
+  //Assigne names provided by user
+  else {
+    firstname = process.argv[2]
+    secondname = process.argv[3]
+  }
+
+  //Console operation in progress
+  console.log(`> Calculating ${randomlove}love ...`)
   let options = {
     method: 'GET',
     url: 'https://love-calculator.p.rapidapi.com/getPercentage',
-    //Store the inserted information in an object
-    params: { fname: process.argv[2], sname: process.argv[3] },
+    //Send the names to the API
+    params: { fname: firstname, sname: secondname },
     headers: {
       'x-rapidapi-key': '0b168600abmsh79f9a6cdaf4fc2ep120417jsn341f4e4f7d29',
       'x-rapidapi-host': 'love-calculator.p.rapidapi.com'
@@ -72,7 +112,7 @@ app.calculateLove = function () {
   //Print the results of love calculation
   axios.request(options).then(function (response) {
     console.log(`-----------------------------------------------------------`)
-    console.log(`\x1b[33m%s\x1b[0m`, ` CALCULATION RESULT:`)
+    console.log(`\x1b[33m%s\x1b[0m`, ` ${randomlove.toUpperCase()}CALCULATION RESULT:`)
     console.log(`\x1b[32m%s\x1b[0m`, ` Percentage of love between ${response.data.fname.toUpperCase()} and ${response.data.sname.toUpperCase()} is: ${response.data.percentage}%`)
     console.log(`\x1b[32m%s\x1b[0m`, ` ${response.data.result}`)
     console.log(`-----------------------------------------------------------`)
@@ -80,7 +120,7 @@ app.calculateLove = function () {
     app.historyfile(response.data.fname.toUpperCase(), response.data.sname.toUpperCase(), response.data.percentage, response.data.result)
   }).then(response => {
     //Send command to be recorded in log file
-    app.logfile(`Calculate love`)
+    app.logfile(`Calculate ${randomlove}love`)
     //Call function to prompt supported commands
     app.consoleOptions()
   })
@@ -93,12 +133,15 @@ app.calculateLove = function () {
 app.consoleOptions = function () {
   console.log(` To continue, please enter the following commands:`)
   console.log(`\x1b[36m%s\x1b[0m`, `   node app.js firstName secondName`, `(To run Love calculator)`)
-  console.log(`\x1b[36m%s\x1b[0m`, `   node app.js history`, `(To print History)`)
+  console.log(`\x1b[36m%s\x1b[0m`, `   node app.js random`, `(To run Love calculator randomly)`)
+  console.log(`\x1b[36m%s\x1b[0m`, `   node app.js history`, `(To print calculation history)`)
   console.log(`-----------------------------------------------------------`)
+
 }
 
-//Validate and execute commands
+//Validate and execute commands provided
 app.runCommand = function () {
+  //Console operation in progress
   console.log(`> Running App...`, '\n')
 
   //Transform the third argument to uppercase
@@ -107,27 +150,44 @@ app.runCommand = function () {
     command2 = command2.toUpperCase()
   }
 
-  //Validate and call function based on the provided command
+  //Validate and call function based on the provided commands
+  //two names where provided
   if (process.argv[2] && process.argv[3] && process.argv.length == 4) {
-    app.calculateLove()
-  } else if (command2 == `HISTORY` && process.argv.length == 3) {
+    if (process.argv[2] == "^[a-zA-Z]+$" && process.argv[3] == "^a-zA-Z]+$") {
+      app.calculateLove()
+    } else {
+      console.log(`-----------------------------------------------------------`)
+      console.log(`\x1b[31m%s\x1b[0m`, ` Oops! Please provide two names.`)
+      console.log(`----------------------------------------------------------- `)
+      //Call function to prompt supported commands
+      app.consoleOptions()
+      //Send command to be recorded in log file
+      app.logfile(`Invalid command`)
+    }
+
+  }
+  //Command is `random`
+  else if (command2 == `RANDOM` && process.argv.length == 3) {
+    app.calculateLove(`random`)
+  } //Command is `history`
+  else if (command2 == `HISTORY` && process.argv.length == 3) {
     app.printHistory()
-  } else {
+  }//Command is not supported 
+  else {
     //Console error message for invalid commands
-    let errMsj = `Command no supported`
+    let errMsj = `Command not supported`
     if (!process.argv[2]) {
       errMsj = 'Please anter a valid command'
     }
-    console.log(`-----------------------------------------------------------`)
-    console.log(`\x1b[31m%s\x1b[0m`, ` Oops! ${errMsj}.`)
-    console.log(`-----------------------------------------------------------`)
+    console.log(`----------------------------------------------------------- `)
+    console.log(`\x1b[31m % s\x1b[0m`, ` Oops! ${errMsj}.`)
+    console.log(`----------------------------------------------------------- `)
     //Call function to prompt supported commands
     app.consoleOptions()
     //Send command to be recorded in log file
     app.logfile(`Invalid command`)
   }
 }
-
 
 //Invoque function to run a command
 app.runCommand()
